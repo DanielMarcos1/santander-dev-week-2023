515cb5d5ed91a66594cbc32eda045226701325ff
DanielMarcos1 <daniel04pessoal@gmail.com>
2023-10-22 15:04:00 -0300

    Gradle test

--- src/main/java/me/dio/Application.java
+++ src/main/java/me/dio/Application.java
@@ -1,14 +1,12 @@
 package me.dio;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class Application {
-
+	@Value("${app.runMode}")
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}

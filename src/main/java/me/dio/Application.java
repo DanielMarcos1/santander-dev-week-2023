89865546d05ae2226a957700b8c4825e1debca66
DanielMarcos1 <daniel04pessoal@gmail.com>
2023-10-22 14:33:42 -0300

    Application adjustment

--- src/main/java/me/dio/Application.java
+++ src/main/java/me/dio/Application.java
@@ -1,12 +1,14 @@
 package me.dio;
 
-import org.springframework.beans.factory.annotation.Value;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class Application {
-	@Value("${app.runMode}")
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
